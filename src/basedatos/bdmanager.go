b83406ecb4e71f9f8d940f56ac4b3d4abe267ff2
CMCFL <claudio.flandez@alumnos.ucn.cl>
2023-12-09 23:16:58 -0300

    implementacion bd

--- src/basedatos/bdmanager.go
+++ src/basedatos/bdmanager.go
@@ -1,1 +1,52 @@
 package basedatos
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const url = "user:password@tcp(localhost:3306)/cartdb"
+
+var db *sql.DB
+
+func Connect() {
+	conexion, err := sql.Open("mysql", url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Conexion")
+	db = conexion
+}
+
+func Close() {
+	fmt.Println("Conexion  terminada")
+	db.Close()
+}
+
+func Ping() {
+	err := db.Ping()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func IsTable() (bool, error) {
+	query := "SELECT table_name FROM information_schema.tables WHERE table_name = ?"
+	var tableName = "carrito"
+	var result string
+	err := db.QueryRow(query, tableName).Scan(&result)
+
+	switch {
+	case err == sql.ErrNoRows:
+		fmt.Println("Tabla no existe")
+		return false, nil // La tabla no existe
+	case err != nil:
+		fmt.Println("Error de Consulta")
+		return false, err // Error al ejecutar la consulta
+	default:
+		fmt.Println("Tabla si existe")
+		return true, nil // La tabla existe
+	}
+}
