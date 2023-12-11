ea5ac4ed811fa65741ea854f66db755f14447d47
CMCFL <claudio.flandez@alumnos.ucn.cl>
2023-12-11 11:10:17 -0300

    aaah

--- src/main.go
+++ src/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"notificationmicroservice/basedatos"
+	"os"
 
 	"log"
 	"net"
 
+	pb "notificationmicroservice/proto"
+
 	_ "github.com/go-sql-driver/mysql"
+	"google.golang.org/grpc"
 	"gorm.io/gorm"
 )
+
+type Server struct {
+	pb.NotificationServiceServer
+}
 
 func main() {
 
@@ -20,10 +29,19 @@ func main() {
 	basedatos.EjecutarMigraciones(bd)
 
 	//gRPC start
-	puerto := "8089"
-	_, err := net.Listen("tcp", ":"+puerto)
+	puerto := "localhost:50051"
+	listener, err := net.Listen("tcp", puerto)
 	if err != nil {
 		log.Fatalf("No se pudo crear Listener: %s", err)
 	}
+
 	fmt.Printf("Servidor activo en puerto %s", puerto)
+	serverNotification := grpc.NewServer()
+
+	if err = serverNotification.Serve(listener); err != nil {
+		log.Fatalf("Error Servidor (?): %s", err)
+	}
+
+	fmt.Printf("Servidor ok %s", puerto)
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
